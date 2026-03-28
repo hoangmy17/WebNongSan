ade2015fc3a4ff40dae0b02ba9771dea7815363b
hoangmy17 <hoangmy1732004@gmail.com>
2026-03-28 10:36:17 +0700

    first commit

---
+++ src/main/java/com/example/WebNongSan/WebNongSanApplication.java
@@ -0,0 +1,13 @@
+package com.example.WebNongSan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebNongSanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebNongSanApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/WebNongSan/WebNongSanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.WebNongSan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebNongSanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
